ee948ca9310f135441824834a1d781ca14f97ab3
Patrick <patr6996@edu.easj.dk>
2020-03-04 13:31:06 +0100

    Implemented button click and TextView update.

--- app/src/main/java/com/example/afleveringsys/MainActivity.java
+++ app/src/main/java/com/example/afleveringsys/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.afleveringsys;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textview;
+    Button button;
+    EditText edittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textview=(TextView) findViewById(R.id.TextViewChange);
+        button=(Button) findViewById(R.id.buttonClick);
+        edittext=(EditText) findViewById(R.id.InputEditText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textview.setText("Hello, button works very well");
+            }
+        });
     }
 }
