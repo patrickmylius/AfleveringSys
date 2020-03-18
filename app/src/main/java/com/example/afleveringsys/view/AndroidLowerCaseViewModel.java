24a71224143da58eb1bc3c524b7264db5000ace4
Patrick <patr6996@edu.easj.dk>
2020-03-18 15:59:33 +0100

    Implemented AndroidLowerCaseViewModel, to transform and present the data in the View.

--- app/src/main/java/com/example/afleveringsys/view/AndroidLowerCaseViewModel.java
+++ app/src/main/java/com/example/afleveringsys/view/AndroidLowerCaseViewModel.java
@@ -1,6 +1,46 @@
 package com.example.afleveringsys.view;
 
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.afleveringsys.model.Model;
+
+import java.util.Observable;
+import java.util.Observer;
 
 public class AndroidLowerCaseViewModel extends ViewModel {
+
+    private MutableLiveData<String> presentableData = new MutableLiveData<>();
+    private Model model = new Model();
+
+    public AndroidLowerCaseViewModel() {
+        observeModel(model);
+        presentableData.setValue(getTransformedData(model.getData()));
+
+    }
+
+    private void observeModel(Model model) {
+        model.addObserver(new Observer() {
+            @Override
+            public void update(Observable o, Object arg) {
+                if (o instanceof Model) {
+                    String data = ((Model) o).getData();
+                    presentableData.setValue(getTransformedData(data));
+                }
+            }
+        });
+    }
+
+    private String getTransformedData(String data) {
+        return data.toLowerCase();
+    }
+
+    public MutableLiveData<String> getPresentableData() {
+        return presentableData;
+    }
+
+    public void setData(String data) {
+        model.setData(data);
+    }
+
 }
---
+++ app/src/main/java/com/example/afleveringsys/view/LowerCasePresenter.java
@@ -0,0 +1,6 @@
+package com.example.afleveringsys.view;
+
+import androidx.lifecycle.ViewModel;
+
+public class LowerCasePresenter extends ViewModel {
+}
